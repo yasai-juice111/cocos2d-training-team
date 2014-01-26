6d58db2d438722424130014e767632fdf56ee921
cocos2d-x <kawabatasatoshi@hotmail.co.jp>
2014-01-26 17:07:32 +0900

    マージして、動く状態に。

---
+++ Classes/KawabataScene.cpp
@@ -0,0 +1,9 @@
+//
+//  KawabataScene.cpp
+//  cocos2dx-space-game
+//
+//  Created by cocos2d-x on 2014/01/26.
+//
+//
+
+#include "KawabataScene.h"
---
+++ Classes/KawabataScene.h
@@ -0,0 +1,23 @@
+//
+//  KawabataScene.h
+//  cocos2dx-space-game
+//
+//  Created by cocos2d-x on 2014/01/26.
+//
+//
+
+#ifndef __cocos2dx_space_game__KawabataScene__
+#define __cocos2dx_space_game__KawabataScene__
+
+#include "StageScene.h"
+
+class KawabataScene : public StageScene
+{
+public:
+    virtual bool init();
+    
+private:
+    
+};
+
+#endif /* defined(__cocos2dx_space_game__KawabataScene__) */
